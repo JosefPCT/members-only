6a098b3ddcbf8eefcf67e7bc930975392ea8d790
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-11-07 07:48:04 +0800

    feat: add basic mvc structure

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+const indexRouter = require('./routes/indexRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World');
-});
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
   console.log("Listening to Port: ", PORT);
