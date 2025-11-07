4e26396be2f0c63a3fc826439d6dafbee6190ed1
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-11-07 12:23:42 +0800

    feat: finish session setup for cookies

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const session = require("express-session");
+const pgStore = require('connect-pg-simple')(session);
 
+const pool = require('./db/pool');
 const indexRouter = require('./routes/indexRoutes');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,8 +13,26 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// Setup the parser middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({extended: true}));
+
+// Session Setup
+const sessionStore = new pgStore({
+  pool: pool,
+  tableName: 'session'
+});
+
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}));
 
 app.use('/', indexRouter);
 
