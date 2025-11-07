5243bfe80cbfe51d9182cea910cadecbcbd039b1
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-11-07 08:02:31 +0800

    chore: refactor views to use partials to create the page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,5 +3,5 @@ const db = require('../db/queries');
 module.exports.getIndex = async(req, res, next) => {
   const data = await db.testQuery();
   console.log(data[0]);
-  res.render('index');
+  res.render('index', {title: 'Home'});
 }
